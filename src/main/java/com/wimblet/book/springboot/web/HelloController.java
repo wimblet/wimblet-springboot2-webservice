535e9edcc29f77a91bcef7c732d0ab70f0c813b7
김민석 <englosia62@gmail.com>
2021-01-13 23:48:03 +0900

    chapter 02 완료

---
+++ src/main/java/com/wimblet/book/springboot/web/HelloController.java
@@ -0,0 +1,22 @@
+package com.wimblet.book.springboot.web;
+
+import com.wimblet.book.springboot.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        System.out.println("헬로");
+        return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
+}
