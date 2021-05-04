e7329dac3081c9ba2600945ab7064a5743bf36d4
김민석 <englosia62@gmail.com>
2021-05-05 01:25:39 +0900

    chapter 05 5.4까지 완료

--- src/main/java/com/wimblet/book/springboot/web/IndexController.java
+++ src/main/java/com/wimblet/book/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.wimblet.book.springboot.web;
 
+import com.wimblet.book.springboot.config.auth.LoginUser;
+import com.wimblet.book.springboot.config.auth.dto.SessionUser;
 import com.wimblet.book.springboot.service.posts.PostsService;
 import com.wimblet.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
@@ -15,9 +19,13 @@ public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
 
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if (user != null) {
+            model.addAttribute("name", user.getName()); // window os 사용시 username을 윈도우의 환경변수 %USERNAME%로 인하여 사용자 이름으로 나오므로 다른 변수를 사용
+        }
         return "index";
     }
 
