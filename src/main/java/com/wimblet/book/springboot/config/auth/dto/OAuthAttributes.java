479cbdcb7be7c3cbb84e85f96ae8b6cbcb89a8a2
김민석 <englosia62@gmail.com>
2021-05-05 17:20:20 +0900

    chapter 05 완료

--- src/main/java/com/wimblet/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/wimblet/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -5,6 +5,7 @@ import com.wimblet.book.springboot.domain.user.User;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Getter
@@ -26,7 +27,21 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if ("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
     }
 
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
